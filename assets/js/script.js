25ff2d0617af6ef7c4b6fd7c529f5defaab362c8
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-09-23 20:46:06 +0800

    COME ON

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function addNavID()
 {
 	var wrapperElement = document.body.getElementsByClassName("wrapper")[0];
 	var sectionElement = wrapperElement.getElementsByTagName("section")[0];
-
+	
 	sectionElement.getElementsByTagName("h2")[0].id = "welcome";
 }
 
