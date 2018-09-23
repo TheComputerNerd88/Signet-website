f3dc38e7ab3ced066da3cb5064f248c95dfed933
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-09-23 20:38:55 +0800

    pls pls work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 function addNavID()
 {
 	var bodyElement = document.getElementsByTagName("body")[0];
-	var wrapperElement = bodyElement.getElementsByClassName("wrapper");
-	var sectionElement = wrapperElement[0].getElementsByTagName("section")[0];
+	var wrapperElement = bodyElement.getElementsByClassName("wrapper")[0];
+	var sectionElement = wrapperElement.getElementsByTagName("section")[0];
 
 	sectionElement.getElementsByTagName("h2")[0].id = "welcome";
 }
