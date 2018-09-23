b9d865b2576930e318918393f03f19771567aaff
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-09-23 19:48:37 +0800

    Fixed JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function addNavID()
 {
 	var bodyElement = document.getElementsByTagName("body")[0];
 	var wrapperElement = bodyElement.getElementsByClassName("wrapper");
-	var sectionElement = wrapperElement.getElementsByTagName("section");
+	var sectionElement = wrapperElement.getElementsByTagName("section")[0];
 
 	sectionElement.getElementsByTagName("h2")[0].id = "welcome";
 }
