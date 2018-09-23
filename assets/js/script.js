8f28180437ebeeb60eb0b7b7eefcb7d9ee2bd43c
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-09-23 20:49:25 +0800

    COME ON!!!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 function addNavID()
 {
-	var wrapperElement = document.body.getElementsByClassName("wrapper")[0];
-	var sectionElement = wrapperElement.getElementsByTagName("section")[0];
-	
-	sectionElement.getElementsByTagName("h2")[0].id = "welcome";
+	if (document.body)
+	{
+		var wrapperElement = document.body.getElementsByClassName("wrapper")[0];
+		var sectionElement = wrapperElement.getElementsByTagName("section")[0];
+
+		sectionElement.getElementsByTagName("h2")[0].id = "welcome";
+	}
 }
 
 for (var i = 0; i < 10; i++)
