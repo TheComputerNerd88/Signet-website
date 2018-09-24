71f3e093b2fee50b5cadaed846c5e0c5518da0db
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-09-24 20:28:38 +0800

    Testing 88888888 - 40!

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ $(window).resize(sectionHeight);
   $("div ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
+    $("div ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
   });
